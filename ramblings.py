6e9e1723cdc3699849dc1db077bc0ec544a2049d
antair <86919729+4ntair@users.noreply.github.com>
2021-10-13 16:46:09 +1000

    Add files via upload

---
+++ ramblings.py
@@ -0,0 +1,51 @@
+def title():
+    print("Mesasoic")
+
+title()
+
+def real_introduction():
+    print("Erik was a autistic teenager that had a infatuation with dinosaurs")
+    print("This infatuation with dinosaurs made him extremely excited to go to the Mesasoic, a new themepark that had opened up")
+    print("which had alot of real dinosaurs. And you've probably also guessed that he hounded his parents to go to this really amazing, cool park")
+    print("The problem being that Parent A and Parent B nad no \"money\" and also they had an inherent fear of getting eaten alive by a Tyrannosaurus Rex")
+    
+real_introduction()
+
+def say_hi():
+    print("Oh Hi,", name)  
+    answer = "Hi"
+    
+name = "Erik"
+say_hi()
+
+def welcome(name='Welcome to', location='Mesasoic'):
+    print('Erik,', name, "the fabulous", location)
+
+welcome()
+
+def enterance():
+    print("As Erik entered the lobby with 1000's of people. The guards and tour guides stood straight and firm like a british guard,")
+    print("it was almost like being paid to be there and act so \"up-standing\". Haha, get it?")
+
+enterance()
+
+def introduction():
+    print("Here we have the largest array of dinosaurs in a amusment park, so much infact. It's the most in the the whole world.")
+    print("This is because we use a foreign technique that uses special sciences to create such large, archaic creatures.")
+    print("(i don't know i'm not a scientist, i am just a tour guide working minimal wage for this god forsaken company that capitalises off of dinosaurs")
+    print("and doesn't care about the ecosystem around them and they went from garage-dwellers to)")
+    print("multi-trillionares and in turn made them greed, narisscistic people with too much mo-.")
+    print("Everyone ignored the guard, although people were in awe by the")
+    print("white, monolithic, contemporary, serrated pillars that the entrance was littered with, making for a minima and contemporary design while also making a linear tunnel to the first attraction.")
+    print("'WOAAAAAH' exclaimed everyone as the hall echoes the echoes that rang furiously in my ears.")
+    print("Anywho said the guard, Let's show you around the park and it's main attractions that give this park worldwide fame and tangible earnings.")
+
+introduction()
+
+def chapter_1():
+    print("Chapter 1: The shortcomings")
+
+chapter_1()
+
+def rising_action():
+    print("Here we all have the exact timeline of the dinosaurs from the")
