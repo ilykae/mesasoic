83f0d103d1493ae45e6de759e20ff98594fcf9ea
kae <86919729+4kae@users.noreply.github.com>
2021-10-16 18:20:29 +1000

    added easier access or whateveer

--- ramblings.py
+++ ramblings.py
@@ -1,20 +1,24 @@
 def title():
-    print("Mesasoic")
+    print("""
+Mesasoic\n
+    """)
 
 title()
 
 def real_introduction():
-    print("Erik was a autistic teenager that had a infatuation with dinosaurs.")
-    print("This infatuation with dinosaurs made him extremely excited to go to the Mesasoic, a new themepark that had opened up literally just yesterday.\n")
-    print("Which had alot of real dinosaurs that moved, interacted with the environment and for once, wasn't light trickery or something of the latter. And you've probably also guessed that he hounded his parentsto go to this really amazing and cool park.")
-    print("The problem being that Parent A and Parent B nad no \"money\". They lied because they didn't want to tell their kid that they had a preconceived fear of getting eaten alive by a Velociraptor.\n")
-    print("OMG!!!, said Parent B.\n")
-    print("WHAT???, said Parent A\n")
-    print("I just got a promotion to CEO of 'Myve', said Parent B.")
-    print("The family was ecstatic to hear and collecitvely said 'HUUURAAAAAAAAAAAH' and all group hugged")
-    print("(for those who don't know. there is a breakout social media just like TikTok in the real world and is hip with the kids nowadays)")
-    print("This promotion lead to the concept of going to the Mesasoic. Erik was estatic to go there even if it meant that there was an insignificantly small chance that it becomes a reality.\n")
-    
+    print("""
+Erik was a autistic teenager that had a infatuation with dinosaurs.
+This infatuation with dinosaurs made him extremely excited to go to the Mesasoic, a new themepark that had opened up literally just yesterday.\n
+Which had alot of real dinosaurs that moved, interacted with the environment and for once, wasn't light trickery or something of the latter. And you've probably also guessed thathe hounded his parentsto go to this really amazing and cool park.
+The problem being that Parent A and Parent B nad no \"money\". They lied because they didn't want to tell their kid that they had a preconceived fear of getting eaten alive by a Velociraptor.\n
+OMG!!!, said Parent B.\n
+WHAT???, said Parent A\n
+I just got a promotion to CEO of 'Myve'!, said Parent B.
+(for those who don't know. there is a breakout social media just like TikTok in the real world and is hip with the kids nowadays)
+The family was ecstatic to hear and collecitvely said 'HUUURAAAAAAAAAAAH' and all group hugged
+This promotion lead to the concept of going to the Mesasoic. Erik was estatic to go there even if it meant that there was an insignificantly small chance that it becomes a reality.\n
+    """)
+
 real_introduction()
 
 def say_hi():
@@ -30,36 +34,43 @@ def welcome(name='Welcome to', location='Mesasoic.\n'):
 welcome()
 
 def chapter_1_real():
-    print("Chapter 1: The Beginning\n")
+    print("""
+Chapter 1: The Beginning\n")
+""")
 
 chapter_1_real()
 
 def enterance():
-    print("As Erik entered the lobby with 1000's of people. He could hear the ear piercing voices the second he entered. The guards and tour guides stood straight and firm like a british guard, all lined up convienientely in the middle of this white haven.")
-    print("\n\"it was almost like being paid to be there and act so \"up-standing\", haha, get it?\" Erik said to himself\n")
-    print("The guards and tour guides stood straight and firm like a british guard, all lined up convienientely in the middle of this white, tasteless hall of purity\n")
-    
-
+    print("""    
+As Erik entered the lobby with 1000's of people. He could hear the ear piercing voices the second he entered. The guards and tour guides stood straight and firm like a british guard, all lined up convienientely in the middle of this white haven.
+\n\"it was almost like being paid to be there and act so \"up-standing\", haha, get it?\" Erik said to himself\n
+The guards and tour guides stood straight and firm like a british guard, all lined up convienientely in the middle of this white, tasteless hall of purity\n
+    """)
 enterance()
 
 def introduction():
-    print("\"Here we have the largest array of dinosaurs in a amusment park,\"\nso much infact. It's the most in the the whole world. said the tourguide in a rather monotone, lifeless voice")
-    print("This is because we use a foreign technique that uses special sciences to create such large, archaic creatures.")
-    print("(i don't know i'm not a scientist, i am just a tour guide working minimal wage for this god forsaken company that capitalises off of dinosaurs")
-    print("and doesn't care about the ecosystem around them and they went from garage-dwellers to")
-    print("multi-trillionares and in turn made them greed, narisscistic people with too much mo-.")
-    print("\nNo one can remotely even hear the guard under his melonchanic mumbles, the people we're more focused on taking in everything even the white, monolithic, contemporary, serrated pillars that the entrance was littered with, making for a minimal and contemporary design\nwhile also making a linear tunnel to the first attraction.\n")
-    print("'WOAAAAAH' exclaimed everyone in awe and tranqiulised by the sheer size of this enterance. The hall echoes rang furiously in my ears.")
-    print("Anywho said the guard, Let's show you around the park and it's main attractions that give this park worldwide fame and tangible earnings. \n")
+    print("""
+\"Here we have the largest array of dinosaurs in a amusment park,\"\nso much infact. It's the most in the the whole world. said the tourguide in a rather monotone, lifeless voice
+This is because we use a foreign technique that uses special sciences to create such large, archaic creatures.
+(i don't know i'm not a scientist, i am just a tour guide working minimal wage for this god forsaken company that capitalises off of dinosaurs
+and doesn't care about the ecosystem around them and it's not my fault that they went from garage-dwellers to
+multi-trillionares and in turn made them greed, narisscistic people with too much mo-.
+\nNo one can remotely even hear the guard under his melonchanic mumbles, the people we're more focused on taking in everything even the white, monolithic, contemporary, serrated pillars that the entrance was littered with, making for a minimal and contemporary design\nwhile also making a linear tunnel to the first attraction.\n
+Anywho said the guard, Let's show you around the park and it's main attractions that give this park worldwide fame and tangible earnings. \n
+    """)
 
 introduction()
 
 def chapter_2():
-    print("Chapter 2:\nWhere things go bad. REALLY BAD.\n")
+    print("""
+Chapter 2:\nWhere things go bad. REALLY BAD.\n
+""")
 
 chapter_2()
 
 def rising_action():
-    print("Here we all have the exact timeline of the dinosaurs from the- 'AHHHHHHHHHHHH' goes everyone as the mysterious creature from the ")
+    print("""
+Here we all have the exact timeline of the dinosaurs from the- 'AHHHHHHHHHHHH' goes everyone as the mysterious creaturearises from the poster. From the observations of Erik, thedinosaur that emerged from the poster ")
+    """)
 
 rising_action()
